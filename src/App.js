c07c389be901f37b9239e1be1d13de31bd505205
hadjev <52852365+hadjev@users.noreply.github.com>
2022-08-10 08:38:11 +0300

    ErrorPage created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Home, Products } from "./pages/index";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
+import ErrorPage from "./pages/ErrorPage.js";
 import Footer from "./components/Footer.js";
 import Navbar from "./components/Navbar.js";
 
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+
+        <Route path="/*" element={<ErrorPage />} />
       </Routes>
       <Footer />
     </Router>
