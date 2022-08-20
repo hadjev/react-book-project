85abf04a9b7855715a1c973365317f83e0f715a5
hadjev <52852365+hadjev@users.noreply.github.com>
2022-08-20 21:26:00 +0300

    login functionality added

--- client/src/components/AuthButton.js
+++ client/src/components/AuthButton.js
@@ -1,32 +1,61 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function AuthButton() {
-  return (
-    <Wrapper className="auth-btn-wrapper">
-      <button className="auth-btn">Login</button>
-    </Wrapper>
-  );
+    const { loginWithRedirect, user, logout } = useAuth0();
+
+    console.log(user);
+
+    return (
+        <Wrapper className="auth-btn-wrapper">
+            {user ? (
+                <>
+                    <button
+                        className="auth-btn"
+                        onClick={() =>
+                            logout({ returnTo: window.location.origin })
+                        }
+                    >
+                        Logout
+                    </button>
+                    <img src={user.picture} alt="" />
+                </>
+            ) : (
+                <button className="auth-btn" onClick={loginWithRedirect}>
+                    Login
+                </button>
+            )}
+        </Wrapper>
+    );
 }
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr auto;
   align-items: center;
+  justify-items: end;
   width: 225px;
 
+  img {
+      width: 50px;
+      border-radius: 50%;
+      margin-left: 10px;
+  }
+  
   .auth-btn {
     display: flex;
     align-items: center;
     background: transparent;
     border-color: transparent;
-    font-size: 1.5rem;
+    font-size: 1rem;
     cursor: pointer;
     color: var(--clr-grey-1);
     letter-spacing: var(--spacing);
     svg {
       margin-left: 5px;
     }
+
 `;
 
 export default AuthButton;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 import { BookProvider } from './context/booksContext.js';
 import { FilterProvider } from './context/filterContext.js';
 import React from 'react';
@@ -8,9 +9,16 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BookProvider>
-        <FilterProvider>
-            <App />
-        </FilterProvider>
-    </BookProvider>
+    <Auth0Provider
+        domain="dev-bimpesij.eu.auth0.com"
+        clientId="msHV0cdPHMr8TlqQSwEiS4rNPdlfl00X"
+        redirectUri={window.location.origin}
+        cacheLocation="localstorage"
+    >
+        <BookProvider>
+            <FilterProvider>
+                <App />
+            </FilterProvider>
+        </BookProvider>
+    </Auth0Provider>
 );
